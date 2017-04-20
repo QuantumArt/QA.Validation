9603118266eba1a46c9b10b7a0c8baecc6921502
Pavel Celut <CelutP@quantumart.ru>
2017-04-20 18:15:08 +0300

    Fix nuget

--- QA.Validation.Xaml.Extensions/Properties/AssemblyInfo.cs
+++ QA.Validation.Xaml.Extensions/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Windows.Markup;
 using QA.Validation.Xaml;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("QA.Validation.Xaml.Extensions")]
@@ -16,8 +16,8 @@ using QA.Validation.Xaml;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -27,15 +27,15 @@ using QA.Validation.Xaml;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyInformationalVersion("1.0.3.0")]
 [assembly: XmlnsDefinition("http://artq.com/validation", "QA.Validation.Xaml.Extensions")]
 [assembly: XmlnsDefinition("http://artq.com/validation", "QA.Validation.Xaml.Extensions.Conditions")]
 [assembly: XmlnsDefinition("http://artq.com/validation", "QA.Validation.Xaml.Extensions.Rules")]
--- QA.Validation.Xaml/Properties/AssemblyInfo.cs
+++ QA.Validation.Xaml/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Windows.Markup;
 using QA.Validation.Xaml;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("QA.Validation.Xaml")]
@@ -16,8 +16,8 @@ using QA.Validation.Xaml;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -27,14 +27,14 @@ using QA.Validation.Xaml;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyInformationalVersion("1.0.3.0")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml", "QA.Validation.Xaml.Markup")]
 [assembly: XmlnsDefinition("http://artq.com/validation", "QA.Validation.Xaml")]
 [assembly: XmlnsDefinition("http://artq.com/validation", "QA.Validation.Xaml.ListTypes")]
